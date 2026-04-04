e54c3165a470908c2d6a8db3b5c33db6770d72aa
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-04-04 16:12:12 +0200

    add autotag

--- usr/lib/enigma2/python/Plugins/Extensions/horoscope/NewOeSk.py
+++ usr/lib/enigma2/python/Plugins/Extensions/horoscope/NewOeSk.py
@@ -23,7 +23,7 @@ __author__ = "Lululla"
 __email__ = "ekekaz@gmail.com"
 __copyright__ = 'Copyright (c) 2024 Lululla'
 __license__ = "GPL-v2"
-__version__ = "1.0"
+from . import __version__
 
 
 def newOE():
